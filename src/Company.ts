186a94bcbe270ebd22499f1487992e95d08772ab
= <=>
2026-02-28 04:53:03 +0000

    company class with random date with faker

---
+++ src/Company.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+export class Company {
+  companyName: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
